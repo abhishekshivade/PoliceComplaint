d6a900fc973c41fec5882c36a4b08601dac0dc10
Abhishek <abhishekshivade@gmail.com>
2024-07-07 23:18:40 +0530

    Desugnation added in Police.cs

--- Models/Police.cs
+++ Models/Police.cs
@@ -6,6 +6,7 @@ namespace WebApplication1.Models
     {
         [Key] public int PoliceId { get; set; }
         [Required,MaxLength(50)] public string Name { get; set; }
+        [Required,MaxLength (50)] public string Designation { get; set; }
         [Required,MaxLength(10)] public int Contact {  get; set; }
         [Required,MaxLength(50)] public string Password {  get; set; }
     }
