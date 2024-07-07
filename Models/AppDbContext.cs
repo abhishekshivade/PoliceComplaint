53100b1ba61367d91133025cc6b190816830e1e1
Abhishek <Abhishek@DESKTOP-GBO6GI6>
2024-07-07 22:18:47 +0530

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        public DbSet<Police> Police { get; set; }
+        public DbSet<Complaint> Complaint { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Complaint>().HasOne(c=>c.AssignedOfficer).WithMany().HasForeignKey(c=>c.AssignedOfficerId);
+        }
+    }
+}
---
+++ Models/Police.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Police
+    {
+        [Key] public int PoliceId { get; set; }
+        [Required,MaxLength(50)] public string Name { get; set; }
+        [Required,MaxLength(10)] public int Contact {  get; set; }
+        [Required,MaxLength(50)] public string Password {  get; set; }
+    }
+}
